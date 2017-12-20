fc4d334584a21d0313345d90c16a2772f3f0ed19
Sparrow.jang <sparrow.jang@gmail.com>
2017-12-20 18:06:00 +0800

    Show cursor when drawing don't use event

--- js/board.js
+++ js/board.js
@@ -637,7 +637,7 @@ DrawingBoard.Board.prototype = {
 		//if the pencil size is big (>10), the small crosshair makes a friend: a circle of the size of the pencil
 		//todo: have the circle works on every browser - it currently should be added only when CSS pointer-events are supported
 		//we assume that if requestAnimationFrame is supported, pointer-events is too, but this is terribad.
-		if (!this._isUsingLoopToRender() && this.ctx.lineWidth > 10 && this.isMouseHovering) {
+		if (this._isUsingLoopToRender() && this.ctx.lineWidth > 10 && this.isMouseHovering) {
 			this.dom.$cursor.css({ width: this.ctx.lineWidth + 'px', height: this.ctx.lineWidth + 'px' });
 			var transform = DrawingBoard.Utils.tpl("translateX({{x}}px) translateY({{y}}px)", { x: this.coords.current.x-(this.ctx.lineWidth/2), y: this.coords.current.y-(this.ctx.lineWidth/2) });
 			this.dom.$cursor.css({ 'transform': transform, '-webkit-transform': transform, '-ms-transform': transform });
